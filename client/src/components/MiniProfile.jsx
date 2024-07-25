92e5f16d915f9cd61186f9cef9c48b3bb215ce68
WishNavy <du.vyshu@gmail.com>
2024-07-25 09:13:19 -0700

    Make a basic miniprofile component without props

---
+++ client/src/components/MiniProfile.jsx
@@ -0,0 +1,34 @@
+const MiniProfile = (props) => {
+
+    return (
+        <div>
+            <img className="profile_picture" src={} alt={} />
+            <span>{}</span>            {/* user name */}
+            <div>
+                <div>
+                    <span>Following</span>
+                    <span>{}</span>    {/* Number of following */}
+                </div>
+                <div>
+                    <span>Followers</span>
+                    <span>{}</span>    {/* Number of followers */}
+                </div>
+            </div>
+            <div>
+                <div>
+                    <span>To Read</span>
+                    <span>{}</span>    {/* Number of to read */}
+                </div>
+                <div>
+                    <span>Reading</span>
+                    <span>{}</span>    {/* Number of reading */}
+                </div>
+                <div>
+                    <span>Read</span>
+                    <span>{}</span>    {/* Number of read */}
+                </div>
+            </div>
+            <span>Your Profile</span>
+        </div>
+    );
+};
