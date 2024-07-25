4884de0d22ff614bba98ab29e2c0cf9900356a64
WishNavy <du.vyshu@gmail.com>
2024-07-25 08:49:21 -0700

    Make a basic book component without props

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -1,1 +1,27 @@
 // individual book view
+const Book = (props) => {
+
+    return (
+        <div>
+            {/* img src comes from props */}
+            <img className="book_cover" src={} alt={} />  
+            <div>
+                <div>
+                    <div>
+                        <span>{}</span>  {/* Book Name */}
+                        <span>{}</span>  {/* Author Name */}
+                    </div>
+                    <div>
+                        <span>{}</span>  {/* Rating */}
+                        <span>{}</span>  {/* avg time spent */}
+                    </div>
+                </div>
+                <div>
+                    <button>Add To My Books</button>   {/* Add Onclick events to buttons */}
+                    <button>Want To Read</button>
+                    <button>Comment</button>
+                </div>
+            </div>
+        </div>
+    );
+};
