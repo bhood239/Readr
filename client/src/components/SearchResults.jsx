7fad6a6636656a4470af7da6e92924c1169d9612
WishNavy <du.vyshu@gmail.com>
2024-07-26 15:12:09 -0700

    Make searchResult component

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -1,1 +1,25 @@
-// children: postForm
+import { useState } from "react";
+import BookList from "./BookList";
+
+const SearchResult = (props) => {
+    const { searchText, setSearchText, searchData } = props;
+    // searchData is the data returned from api
+    const [searchResult, setSearchResult] = useState([]);
+
+    const handleClick = () => {
+        setSearchResult(searchData);
+    };
+
+    return(
+        <div>
+            <div>
+                <input type="text" value={searchText} onChange={e => setSearchText(e.target.value)} placeholder="Search" />
+                <button onClick={() => handleClick()}>Search</button>
+            </div>
+            
+            <BookList books={searchResult} />
+        </div>
+    );
+};
+
+export default SearchResult;
