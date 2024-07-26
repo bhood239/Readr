938b74e25766f4202758013ea34289bae01d787e
WishNavy <du.vyshu@gmail.com>
2024-07-26 11:41:17 -0700

    Add necessary comments

--- client/src/components/MiniProfile.jsx
+++ client/src/components/MiniProfile.jsx
@@ -1,5 +1,6 @@
 const MiniProfile = (props) => {
     const { user, toRead, reading, read, loadProfile } = props;
+    // toRead, reading and read are states containing array of book details object
     const numOfToRead = toRead.length;
     const numOfReading = reading.length;
     const numOfRead = read.length;
@@ -7,31 +8,32 @@ const MiniProfile = (props) => {
     return (
         <div>
             <img className="profile_picture" src={user.profile_photo} alt={user.username} />
-            <span>{user.username}</span>            {/* user name */}
+            <span>{user.username}</span>          
             <div>
                 <div>
                     <span>Following</span>
-                    <span>{user.following}</span>    {/* Number of following */}
+                    <span>{user.following}</span>    
                 </div>
                 <div>
                     <span>Followers</span>
-                    <span>{user.followers}</span>    {/* Number of followers */}
+                    <span>{user.followers}</span>   
                 </div>
             </div>
             <div>
                 <div>
                     <span>To Read</span>
-                    <span>{numOfToRead}</span>    {/* Number of to read */}
+                    <span>{numOfToRead}</span>    
                 </div>
                 <div>
                     <span>Reading</span>
-                    <span>{numOfReading}</span>    {/* Number of reading */}
+                    <span>{numOfReading}</span>    
                 </div>
                 <div>
                     <span>Read</span>
-                    <span>{numOfRead}</span>    {/* Number of read */}
+                    <span>{numOfRead}</span>    
                 </div>
             </div>
+            {/* loadProfile is a function which will render ProfilePage in App.jsx */}
             <span onClick={() => loadProfile()}>Your Profile</span>
         </div>
     );
--- client/src/helpers/hooks/useApplicationData.jsx
+++ client/src/helpers/hooks/useApplicationData.jsx
@@ -1,1 +1,6 @@
 // in-app states
+// states for toRead, reading, read and favBooks (array of book details objects)
+// loadProfile (function to render profile on App.jsx) -> This may go in App.jsx
+// functions for books - addWantToRead, addReading, addRead, addFav, removeFav
+// functions for post - addPost, removePost
+// functions for HomePage - login, signup, logout
