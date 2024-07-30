01282be74fe208183cfb586b29a5acb77b546273
Ben Hood <benhood97@btinternet.com>
2024-07-30 16:56:03 -0700

    resolve merge conflicts

--- client/src/components/TopNavBar.jsx
+++ client/src/components/TopNavBar.jsx
@@ -1,47 +1,41 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import { FaSearch } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
 
-
-
-const TopNavBar = ({setCurrentPage, user, handleLogout }) => {
-
-
-
+const TopNavBar = ({ user, handleLogout }) => {
   return (
     <Navbar bg="light" variant="light" className="navbar-underline">
       <Navbar.Brand>Readr</Navbar.Brand>
       <Nav className="mr-auto">
         {user ? (
           <>
+            <LinkContainer to="/dashboard">
+              <Nav.Link>Dashboard</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/profile">
+              <Nav.Link>Profile</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/search">
+              <Nav.Link>Search <FaSearch /></Nav.Link>
+            </LinkContainer>
             <Nav.Item>
-              <Nav.Link  onClick={() => setCurrentPage('dashboard')}>Dashboard</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link  onClick={() => setCurrentPage('profile')}>Profile</Nav.Link>
-            </Nav.Item>
-           <Nav.Item>
-            <Nav.Link  onClick={() => setCurrentPage('search')}>Search <FaSearch/></Nav.Link>
-           </Nav.Item>
-            <Nav.Item>
-              <Nav.Link onClick={ handleLogout}>Logout</Nav.Link>
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
             </Nav.Item>
           </>
         ) : (
           <>
-            <Nav.Item>
-              <Nav.Link onClick={() => setCurrentPage('homepage')}>Login</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link onClick={() => setCurrentPage('homepage')}>Register</Nav.Link>
-            </Nav.Item>
+            <LinkContainer to="/">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/">
+              <Nav.Link>Register</Nav.Link>
+            </LinkContainer>
           </>
         )}
       </Nav>
     </Navbar>
   );
-
-
-}
+};
 
 export default TopNavBar;
