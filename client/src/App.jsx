77d4ee8fe13b5c3a8405c2df1233790b568e152d
Ben Hood <benhood97@btinternet.com>
2024-07-29 21:42:37 -0700

    change app component from class based to functional && implement basic react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,22 @@
 // to include: TopNavBar, Footer, conditionally render: Homepage, Dashboard
-import React, { Component } from "react";
-import axios from "axios";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import TopNavBar from "./components/TopNavBar";
+import Footer from "./components/Footer";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: "Click the button to load data!",
-    };
-  }
-
-  fetchData = () => {
-    axios
-      .get("/api/data") // You can simply make your requests to "/api/whatever you want"
-      .then((response) => {
-        // handle success
-        console.log(response.data); // The entire response from the Rails API
-
-        console.log(response.data.message); // Just the message
-        this.setState({
-          message: response.data.message,
-        });
-      });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <h1>{this.state.message}</h1>
-        <button onClick={this.fetchData}>Fetch Data</button>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <TopNavBar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/profile" component={Profile} />
+      </Switch>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
