1a4f89e71ef9e92b252319ea54718c9444644712
WishNavy <du.vyshu@gmail.com>
2024-08-05 16:58:06 -0700

    update book_id datatype to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_02_005622) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_05_235537) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,7 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_02_005622) do
     t.integer "rating", null: false
     t.string "review"
     t.integer "time_spent"
-    t.integer "book_id", null: false
+    t.string "book_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
