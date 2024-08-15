30b77e80073cfbbef8b73064d68ed41c0738518c
Ben Hood <benhood97@btinternet.com>
2024-08-14 19:09:01 -0700

    change cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://ben-deployment--shimmering-mandazi-3b4f4e.netlify.app', 'http://localhost:3000'
+    origins 'https://shimmering-mandazi-3b4f4e.netlify.app/', 'http://localhost:3000'
 
     resource '*',
       headers: :any,
